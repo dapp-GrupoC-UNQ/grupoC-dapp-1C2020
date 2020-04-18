93c26521903459221100bedf89066d77dfb58675
LucianaAlonso <luliialonso@hotmail.com>
2020-04-17 21:52:30 -0300

    se agrega cliente

--- src/main/java/model/Cliente.java
+++ src/main/java/model/Cliente.java
@@ -1,4 +1,17 @@
 package model;
 
+import model.excepciones.NombreDeUsuarioInvalidoException;
+
 public class Cliente {
+
+    private String nombreUsuario;
+
+    public Cliente(String usuario){
+        if(usuario.isEmpty()){
+            throw new NombreDeUsuarioInvalidoException();
+        }
+        nombreUsuario = usuario;
+    }
+
+    public String usuario() { return this.nombreUsuario;   }
 }
--- src/test/java/dominio/ClienteTest.java
+++ src/test/java/dominio/ClienteTest.java
@@ -1,4 +1,23 @@
 package dominio;
 
+import model.Cliente;
+import builders.ClienteBuilder;
+import model.excepciones.NombreDeUsuarioInvalidoException;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class ClienteTest {
+
+    @Test
+    public void clienteTieneUnUsuario(){
+        Cliente pepe = ClienteBuilder.unCliente().conUsuario("pepe1234").build();
+        assertEquals(pepe.usuario(), "pepe1234");
+    }
+
+    @Test
+    public void clienteNoPuedeTenerUnUsuarioSinNombre(){
+        assertThrows(NombreDeUsuarioInvalidoException.class , ()->  ClienteBuilder.unCliente().conUsuario("").build() );
+    }
 }
