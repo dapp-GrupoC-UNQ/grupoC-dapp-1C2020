a7c0da28f45719ab918b6aeb83730248c794154a
belu <belen.amat29@gmail.com>
2020-04-18 23:55:35 -0300

    login inicial

---
+++ frontend/src/scss/pages/homepage/HomePage.js
@@ -0,0 +1,34 @@
+import {withRouter} from "react-router-dom";
+import * as React from "react";
+
+class LoginPage extends React.Component {
+    render() {
+        return (
+            <div className="login-page">
+                <div className="login-panel">
+                    <div className="images-subpanel">
+                    </div>
+                    <div className="divisor-line">
+                    </div>
+                    <div className="user-subpanel">
+                        <div className="welcome-text">
+                            ¡Bienvenidx!
+                        </div>
+                        <div className="lets-buy-text">
+                            Logeate con tu usuario y contraseña y empezá a comprar tus productos favoritos
+                        </div>
+                        <div className="usuario-input">
+                            <label htmlFor="fname">Usuario:</label>
+                            <input type="text" id="fname" name="fname"/>
+                        </div>
+                        <div className="password-input">
+                            <label htmlFor="fname">Contraseña:</label>
+                            <input type="password" id="password" name="password"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default withRouter(LoginPage);
