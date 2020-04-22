33102f7cc2c19eb2959a5693e3db67b9c5a41763
belu <belen.amat29@gmail.com>
2020-04-21 21:13:42 -0300

    agrego campos de horarios al modal de registro de comercio

--- frontend/src/pages/loginpage/modalRegistro/CamposRegistroComercio.js
+++ frontend/src/pages/loginpage/modalRegistro/CamposRegistroComercio.js
@@ -1,13 +1,28 @@
 import * as React from "react";
-import HorarioApertura from "./HorarioApertura";
 
 class CamposRegistroComercio extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            agregarHorario: true
+        }
+    }
+
+    textoHorarios = () => this.state.agregarHorario ? 'agregar' : 'quitar';
+
+    actualizarTextoHorarios = () => this.setState({agregarHorario: !this.state.agregarHorario})
     render() {
         return (<div className="modal-card-body is-flex">
                     <div className="seccion-de-campos">
                         <div className="campo-a-rellenar">
                             <label>
                                 Nombre de tu comercio
+                            </label>
+                            <input type="text" id="nombreYApellido" name="nombreYApellido"/>
+                        </div>
+                        <div className="campo-a-rellenar">
+                            <label>
+                                Direccion
                             </label>
                             <input type="text" id="nombreYApellido" name="nombreYApellido"/>
                         </div>
@@ -26,11 +41,27 @@ class CamposRegistroComercio extends React.Component {
                     </div>
 
                 <div className="seccion-de-campos">
-                    <div className="campo-a-rellenar horarios">
+                    <div className="campo-a-rellenar">
                         <label>
                             Horarios de apertura
                         </label>
-                        <input type="text" name="nombreYApellido"/>
+                        <div className="horarios">
+                            <div className="seleccion-horario">
+                                <input type="time" name="nombreYApellido"/> a
+                                <input type="time" name="nombreYApellido"/>
+                            </div>
+
+                            {!this.state.agregarHorario &&
+                            <div>
+                                <input type="time" name="nombreYApellido"/> a
+                                <input type="time" name="nombreYApellido"/>
+                            </div>
+                            }
+
+                            <div className='botonera-horarios'>
+                                <a onClick={this.actualizarTextoHorarios}>{this.textoHorarios()} horario</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
