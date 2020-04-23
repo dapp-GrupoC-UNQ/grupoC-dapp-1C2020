c1daca543d47455f379660c6a4be8e62ff9094db
belu <belen.amat29@gmail.com>
2020-04-23 00:01:29 -0300

    refactors

--- frontend/src/pages/homepage/HomePage.js
+++ frontend/src/pages/homepage/HomePage.js
@@ -5,38 +5,41 @@ import "./homepage.scss"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 class HomePage extends React.Component {
+    comercios = [{
+        nombreComercio: "No hay por qué",
+        distanciaComercio: '2.5km',
+        rubrosComercio: 'carniceria, limpieza, verduleria'
+    },
+        {
+            nombreComercio: "Lo de tito",
+            distanciaComercio: '0.9km',
+            rubrosComercio: 'carniceria, verduleria'
+        }
+    ]
     render() {
         return(
             <div className="homepage">
                 <div className="comercios">
-                    <div className="carta-comercio">
-                        <div className='imagen-comercio'>
-                            <img src={'https://www.memesargentinos.com.ar/wp-content/uploads/2019/02/China-Supermercado.jpg'}/>
-                        </div>
-                        <div className='nombre-comercio'>
-                            Super "No hay por qué"
-                        </div>
-                        <div className='distancia-comercio'>
-                            <FontAwesomeIcon icon={faMapMarkerAlt}/>
-                            <p className="distancia">a 2.5 km</p>
-                        </div>
-                        <div className="rubros-comercio">
-                            <FontAwesomeIcon icon={faStore}/>
-                            <p className="rubros">carniceria, limpieza, verduleria</p>
-                        </div>
-                    </div>
-                    <div className="carta-comercio">
-                        hi
-                    </div>
-                    <div className="carta-comercio">
-                        hi
-                    </div>
-                    <div className="carta-comercio">
-                        hi
-                    </div>
-                    <div className="carta-comercio">
-                        hi
-                    </div>
+                    {this.comercios.map(comercio => {
+                        return (
+                            <div className="carta-comercio">
+                                <div className='imagen-comercio'>
+                                    <img src={'https://www.memesargentinos.com.ar/wp-content/uploads/2019/02/China-Supermercado.jpg'}/>
+                                </div>
+                                <div className='nombre-comercio'>
+                                    {comercio.nombreComercio}
+                                </div>
+                                <div className='distancia-comercio'>
+                                    <FontAwesomeIcon icon={faMapMarkerAlt}/>
+                                    <p className="distancia">{comercio.distanciaComercio}</p>
+                                </div>
+                                <div className="rubros-comercio">
+                                    <FontAwesomeIcon icon={faStore}/>
+                                    <p className="rubros">{comercio.rubrosComercio}</p>
+                                </div>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         )
