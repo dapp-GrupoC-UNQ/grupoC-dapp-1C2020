e1f624d33ea26766b79364b7eb1667e55f745861
belu <belen.amat29@gmail.com>
2020-04-22 21:24:03 -0300

    agrego checkboxes de rubros al comercio

--- frontend/src/pages/loginpage/modalRegistro/ModalRegistroUsuario.js
+++ frontend/src/pages/loginpage/modalRegistro/ModalRegistroUsuario.js
@@ -1,7 +1,6 @@
 import * as React from "react";
 import "./modalregistrousuario.scss"
 import CamposRegistroUsuario from "./CamposRegistroUsuario";
-import CamposRegistroComercio from "./CamposRegistroComercio";
 
 class ModalRegistroUsuario extends React.Component {
     constructor(props) {
@@ -15,6 +14,18 @@ class ModalRegistroUsuario extends React.Component {
     textoBotonRegistrarmeComo = () => this.state.registrandoUsuario ? 'comercio' : 'usuario';
 
     cambiarDeEntidad = () => this.setState({registrandoUsuario: !this.state.registrandoUsuario})
+
+    agregarRubro = (rubro) => {
+        let listaActualRubros = (this.state.rubros || [])
+        //agregar a la lista  si se selecciona. Si se deselecciona hay que sacarlo
+        let nuevosRubros = listaActualRubros.includes(rubro) ? listaActualRubros.filter(rubroEnLista => rubroEnLista !== rubro) : listaActualRubros.concat(rubro)
+        this.setState({rubros: nuevosRubros})
+    }
+
+    actualizarFormulario = (clave, valor) => {
+        this.setState({[clave]: valor})
+    }
+
     render() {
         return(
             <div className="modal">
@@ -24,8 +35,9 @@ class ModalRegistroUsuario extends React.Component {
                         <p className="modal-card-title">Registrate como {this.entidadARegistrar()}</p>
                         <button className="delete" aria-label="close" onClick={this.props.onClose}></button>
                     </header>
-                        {this.state.registrandoUsuario && <CamposRegistroUsuario/>}
-                        {!this.state.registrandoUsuario && <CamposRegistroComercio/>}
+                        <CamposRegistroUsuario onUpdate={this.actualizarFormulario}
+                                               onAgregarRubro={this.agregarRubro}
+                                               esComercio={!this.state.registrandoUsuario}/>
                     <footer className="modal-card-foot">
                         <button className="boton-modal">Registrarme</button>
                         <button className="boton-modal" onClick={this.cambiarDeEntidad}>Registrarme como {this.textoBotonRegistrarmeComo()}</button>
