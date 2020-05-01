8c323664c55fd725d575e586fd4598eb58425677
belu <belen.amat29@gmail.com>
2020-05-01 13:13:05 -0300

    extraigo SideBar a componente externo para usarlo desde la home page

---
+++ frontend/src/pages/homepage/side-bar/SideBar.js
@@ -0,0 +1,32 @@
+import * as React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faArrowCircleRight} from "@fortawesome/free-solid-svg-icons";
+import "./side-bar.scss"
+
+class SideBar extends React.Component {
+    render() {
+        return (
+            <div className="side-bar">
+                <div className="bar-title">
+                    Busca tu producto
+                </div>
+                <div className="links-container">
+                    <div className="link">
+                        <a className="link-search" onClick={this.props.showStores}>Comercios</a>
+                        <FontAwesomeIcon icon={faArrowCircleRight}/>
+                    </div>
+                    <div className="link">
+                        <a className="link-search" onClick={this.props.showCategories}>Rubro</a>
+                        <FontAwesomeIcon icon={faArrowCircleRight}/>
+                    </div>
+                    <div className="link">
+                        <a className="link-search" onClick={this.props.showDiscounts}>Ofertas</a>
+                        <FontAwesomeIcon icon={faArrowCircleRight}/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default  SideBar
