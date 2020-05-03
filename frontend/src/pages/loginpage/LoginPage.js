6d3b07762502e92020d30ef4e02562e0eb66d600
belu <belen.amat29@gmail.com>
2020-05-02 22:13:09 -0300

    agrego validaciones con colores a los campos de login. Creo el login service para pegarle al backend a la hora de validar un usuario

--- frontend/src/pages/loginpage/LoginPage.js
+++ frontend/src/pages/loginpage/LoginPage.js
@@ -2,18 +2,37 @@ import {withRouter} from "react-router-dom";
 import * as React from "react";
 import ImagenesProductos from "./imagenes-home-page/ImagenesProductos";
 import ModalRegistroUsuario from "./modalRegistro/ModalRegistroUsuario";
+import LoginService from "../../servicios/LoginService";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faExclamationTriangle} from "@fortawesome/free-solid-svg-icons";
 
 class LoginPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            userRegisterModalOpen: false
+            userRegisterModalOpen: false,
+            userFieldsError: false,
+            username: '',
+            password: ''
         }
     }
 
     closeModal = () => {
         this.setState({userRegisterModalOpen: false})
     }
+
+    loginUser = () => {
+        if (this.userIsValid()) {
+            LoginService().validateUser({username: this.state.username, password: this.state.password})
+                .then(response => this.props.history.push("/homepage"))
+        }
+    }
+
+    userIsValid = () => {
+        this.setState({userFieldsError: (!this.state.username || !this.state.password) })
+        return (!!this.state.username && this.state.username !== "") || (!!this.state.password && this.state.password !== "")
+    }
+
     render() {
         return (
             <div className="login-page">
@@ -30,21 +49,27 @@ class LoginPage extends React.Component {
                         <div className="lets-buy-text">
                             Logeate con tu usuario y contraseña y empezá a comprar tus productos favoritos
                         </div>
-                        <div className="usuario-input">
+                        <div className={"usuario-input" + (this.state.userFieldsError ? " error" : "")}>
                             <label htmlFor="fname">Usuario:</label>
-                            <input type="text" id="fname" name="fname"/>
+                            <input type="text" id="fname" name="fname" onChange={(event) => this.setState({username: event.target.value})}/>
                         </div>
-                        <div className="password-input">
+                        <div className={"password-input" + (this.state.userFieldsError ? " error" : "")}>
                             <label htmlFor="fname">Contraseña:</label>
-                            <input type="password" id="password" name="password"/>
+                            <input type="password" id="password" name="password" onChange={(event) => this.setState({password: event.target.value})}/>
                         </div>
+                        {this.state.userFieldsError &&
+                        <div className="uncompleted-fields-error">
+                            <FontAwesomeIcon icon={faExclamationTriangle}/>
+                            Hay campos sin completar.
+                        </div>
+                        }
                         <div className="button-panel">
                             <div className="login-button-field">
                                 <button className="login-button"
                                         onClick={() => this.setState({userRegisterModalOpen: true})}>¿No tenes cuenta?</button>
                             </div>
                             <div className="login-button-field">
-                                <button className="login-button" onClick={() => this.props.history.push("/homepage")}>¡Vamos!</button>
+                                <button className="login-button" onClick={this.loginUser}>¡Vamos!</button>
                             </div>
                         </div>
 
