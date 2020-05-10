674f6348bedf8778d89b5453013b91857c54859d
belu <belen.amat29@gmail.com>
2020-05-10 16:11:48 -0300

    intento numero 2 de romper travis

--- backend/src/test/java/com/example/demo/DemoApplicationTests.java
+++ backend/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.Assert.assertTrue;
+
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(false);
 	}
 
 }
