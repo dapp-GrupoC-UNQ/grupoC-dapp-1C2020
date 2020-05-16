a36e82915d95826d87497837c665d685a910e528
belu <belen.amat29@gmail.com>
2020-05-16 16:55:12 -0300

    Agrego boton de carrito a los productos

--- frontend/src/pages/homepage/product/Product.js
+++ frontend/src/pages/homepage/product/Product.js
@@ -1,8 +1,16 @@
 import * as React from "react";
+import "./product.scss"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faShoppingCart} from "@fortawesome/free-solid-svg-icons";
 
 class Product extends React.Component {
     render() {
         return (<div className="entity-card product-card">
+            <div className="product-button">
+                <button className="add-to-cart-button">
+                    <FontAwesomeIcon icon={faShoppingCart}/>
+                </button>
+            </div>
             <div className='imagen-comercio'>
                 <img src={this.props.product.productImage}/>
             </div>
