d666f2a2a0cfb94135186b591f751e67188da193
LucianaAlonso <luliialonso@hotmail.com>
2020-06-13 15:53:05 -0300

    query para obtener mercaderia de un store

--- backend/src/test/java/com/example/demo/repositories/MerchandiseRepositoryTest.java
+++ backend/src/test/java/com/example/demo/repositories/MerchandiseRepositoryTest.java
@@ -1,7 +1,9 @@
 package com.example.demo.repositories;
 
 import com.example.demo.builders.MerchandiseBuilder;
+import com.example.demo.builders.StoreBuilder;
 import com.example.demo.model.merchandise.Merchandise;
+import com.example.demo.model.store.Store;
 import com.example.demo.repositories.merchandise.MerchandiseRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,6 +24,9 @@ public class MerchandiseRepositoryTest {
     @Autowired
     private MerchandiseRepository merchandiseRepository;
 
+    @Autowired
+    private StoreRepository storeRepository;
+
     @Test
     public void aMerchandiseIsRetrieved() {
         Merchandise merchandise = MerchandiseBuilder.aMerchandise().build();
@@ -27,5 +34,19 @@ public class MerchandiseRepositoryTest {
 
         Optional<Merchandise> retrievedMerchandise = merchandiseRepository.findById(merchandise.id());
         assertThat(retrievedMerchandise).hasValue(merchandise);
+    }
+
+    @Test
+    public void getMerchandiseFromSpecificStore() {
+        Store store = StoreBuilder.aStore().buildWithId();
+        Merchandise fideos = MerchandiseBuilder.aMerchandise().withName("Fideos").build();
+        Merchandise arroz = MerchandiseBuilder.aMerchandise().withName("Arroz").build();
+        storeRepository.save(store);
+        merchandiseRepository.save(fideos);
+        merchandiseRepository.save(arroz);
+        List<Merchandise> merchandiseList = Arrays.asList(fideos);
+
+        Optional<List<Merchandise>> retrivedMerchandiseList = merchandiseRepository.getMerchandiseFromStore(store.id());
+        assertThat(retrivedMerchandiseList).hasValue(merchandiseList);
     }
 }
